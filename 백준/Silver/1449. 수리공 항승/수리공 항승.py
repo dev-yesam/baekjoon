22a4c77d6f47e7a952301a0273e5c07a719d3c55
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-01-26 19:48:49 +0900

    [Silver III] Title: 수리공 항승, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1449. 수리공 항승/수리공 항승.py
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.py
@@ -1,22 +1,19 @@
-# 물이 새는 곳의 개수 N과 테이프 길이 L, 물이 새는 곳의 위치 입력
-N, L = map(int, input().split())
-L_lst = list(map(int, input().split()))
+#1449
 
+n, l = map(int, input().split())
 
-# 리스트 정렬
-L_lst.sort()
-start=L_lst[0]
+locations = list(map(int, input().split()))
+locations.sort()
+location = locations[0]
 cnt = 1
 
-# 물 새는 곳 하나 하나 막기
-for i in L_lst[1:]:
-    if i <= start + L-1:
-        continue
+for idx in range(1, len(locations)):
+    if locations[idx] <= location+l-1:
+        pass
     else:
-        start = i
-        cnt +=1
+        location = locations[idx]
+        cnt+=1
 
 print(cnt)
 
-
-
+    
