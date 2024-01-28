9161a776d3891acdbfc465eebb0bb9454557461a
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-01-28 20:06:17 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 184 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.py
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.py
@@ -10,23 +10,16 @@ for _ in range(t):
 
     # 내림차순 정렬
     lst.sort(reverse=True)
-
+    #10 02 13 24 35....n-1.n-3, n,n-2, n.n-1
     # 돌면서 넣기
     res = 0
-    new_lst=[lst[0]]
-    for i in range(1,len(lst)):
-        if i%2 ==1:
-            new_lst.insert(0,lst[i])
-            if res < new_lst[1]-new_lst[0]:
-                res = new_lst[1]-new_lst[0]
-
-        else:
-            new_lst.append(lst[i])
-            if res < new_lst[-2]-new_lst[-1]:
-                res =new_lst[-2]-new_lst[-1]
-    
-    if res < abs(new_lst[0]-new_lst[-1]):
-        res=abs(new_lst[0]-new_lst[-1])
+    if abs(lst[1]-lst[0])>abs(lst[n-1]-lst[n-2]):
+        res = abs(lst[1]-lst[0])
+    else:
+        res = abs(lst[n-1]-lst[n-2])
+    for i in range(2,n):
+        dif= abs(lst[i]-lst[i-2])
+        if dif > res:
+            res = dif
     
     print(res)
-
