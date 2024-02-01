d83e36b0b074174ddc4e46ded21c7a35453ed872
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-02-01 15:46:00 +0900

    [Silver I] Title: 물병, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1052. 물병/물병.py
+++ 백준/Silver/1052. 물병/물병.py
@@ -1,47 +1,17 @@
-# 1052
+# 1052 물병
 
-
-# N : 현재 가지고 있는 물병
-# K : 최종적으로 K 이하의 물병 만들어야 함.
-# B : 사야하는 물병 수 => 최저 구해야함.
-
-
-# N, K 입력 받음
+# 입력
 N, K = map(int, input().split())
 
-# 가지고 있는 물병수 다 합치면, 2진수로 110 이렇게 표현 됨. 4리터 하나, 2리터 하나.
-# 비트 연산 개수에 맞춰줘야 겠네.
-# 1. 2진수에서 1의 개수 카운팅.(k보다 큰지)
-cnt =0
-B = 1
+# 2진수로 바꾼 현재 물병의 수(2진수 1의 수)
+water_bottle = bin(N).count('1')
 
-N2= N
-while N > 0:
-    if N & B :
-        cnt +=1
-        N -= B
-    B *= 2
+new = N  # 이후 물병의 총 수(10진수)
+cnt = 0  # 추가할 물병의 총 수(10진수)
+bottle = 0  # 추가할 물병
+while bin(new).count('1') > K:
+    bottle = 2 ** bin(new)[::-1].index('1')
+    new += bottle
+    cnt += bottle
 
-
-if cnt <= K :
-    print(0)
-
-# 2. K보다 많다면, 2진수 뒤에서 ...
-else:
-    B2 = 1
-    cnt = cnt-K
-    C2 = 0
-    while N2 > B2:
-        if N2 & B2:
-            if cnt ==0:
-                res=B2-C2
-            else:
-                C2 += B2
-
-
-            cnt -= 1
-        B2 *= 2
-
-
-    print(res)
-
+print(cnt)
