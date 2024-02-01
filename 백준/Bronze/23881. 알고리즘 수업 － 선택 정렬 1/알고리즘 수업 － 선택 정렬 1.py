1786aef9d8c9991ad33d39ed569e86416eca2d8f
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-02-01 22:16:25 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 1, Time: 208 ms, Memory: 110620 KB -BaekjoonHub

---
+++ 백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+# k번째 교환되는 수 = 오름차순 정렬일 때 k-1 인덱스의 원소.
+n, k = map(int, input().split())
+lst = list(map(int, input().split()))
+cnt = 0 # 교환 횟수
+
+for i in range(n-1, 0,-1):
+    max_idx = i
+    for j in range(0,i):
+        if lst[max_idx] < lst[j] :
+            max_idx = j
+    if max_idx != i:
+        lst[i], lst[max_idx] = lst[max_idx], lst[i]
+        cnt += 1
+        if cnt == k:
+            print(lst[max_idx], lst[i])
+            break
+if cnt < k:
+    print(-1)
+
