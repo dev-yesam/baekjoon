877874ffb01851fbfbcea283d5c3ee5219761193
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-02-01 21:13:45 +0900

    [Silver IV] Title: 보물, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.py
+++ 백준/Silver/1026. 보물/보물.py
@@ -2,16 +2,11 @@ n = int(input())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
-# B 복사하기
-C = B[:]
-
-# 정렬하기
-A.sort()
-C.sort(reverse=True)
-
-# 최솟값 계산
 res = 0
+
 for i in range(n):
-    res += A[i] * C[i]
-    
+    res += max(A)*min(B)
+    A.remove(max(A))
+    B.remove(min(B))
+
 print(res)
