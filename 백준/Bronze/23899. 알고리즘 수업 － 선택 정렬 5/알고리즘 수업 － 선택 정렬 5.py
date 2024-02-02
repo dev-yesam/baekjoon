b6cabe685de79a9b53eb146b839f7c13a014dd05
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-02-02 20:54:30 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 5, Time: 220 ms, Memory: 111076 KB -BaekjoonHub

--- 백준/Bronze/23899. 알고리즘 수업 － 선택 정렬 5/알고리즘 수업 － 선택 정렬 5.py
+++ 백준/Bronze/23899. 알고리즘 수업 － 선택 정렬 5/알고리즘 수업 － 선택 정렬 5.py
@@ -12,9 +12,9 @@ for i in range(N - 1, 0, -1):
         break
 
     largest = i
-    for j in range(i + 1):
+    for j in range(i):
         if A_lst[j] > A_lst[largest]:
             largest = j
     A_lst[i], A_lst[largest] = A_lst[largest], A_lst[i]
 
-print(1 if A_lst == B_lst else 0)
+print(1 if A_lst ==B_lst else 0)
