cacb2d4a4ea8c6482242966f76dfec46a7a9e388
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-02-02 20:49:42 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 5, Time: 220 ms, Memory: 111076 KB -BaekjoonHub

---
+++ 백준/Bronze/23899. 알고리즘 수업 － 선택 정렬 5/알고리즘 수업 － 선택 정렬 5.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+# 입력
+N = int(input())
+A_lst = list(map(int, input().split()))
+B_lst = list(map(int, input().split()))
+
+# 선택 정렬
+for i in range(N - 1, 0, -1):
+    if A_lst == B_lst:
+        break
+
+    largest = i
+    for j in range(i + 1):
+        if A_lst[j] > A_lst[largest]:
+            largest = j
+    A_lst[i], A_lst[largest] = A_lst[largest], A_lst[i]
+
+print(1 if A_lst == B_lst else 0)
