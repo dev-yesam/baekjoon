943caf27d7da247ade97acc4eedf2c2f34977b81
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-02-02 21:10:39 +0900

    [Bronze I] Title: 알고리즘 수업 - 버블 정렬 2, Time: 384 ms, Memory: 110820 KB -BaekjoonHub

---
+++ 백준/Bronze/23969. 알고리즘 수업 － 버블 정렬 2/알고리즘 수업 － 버블 정렬 2.py
@@ -0,0 +1,15 @@
+n, k = map(int, input().split())
+lst = list(map(int, input().split()))
+cnt = 0
+for i in range(n-1,0,-1):
+    for j in range(i):
+        if lst[j] > lst[j+1]:
+            cnt +=1
+            lst[j], lst[j+1] = lst[j+1], lst[j]
+
+            if cnt == k:
+                print(*lst)
+                exit()
+
+if cnt < k:
+    print(-1)
