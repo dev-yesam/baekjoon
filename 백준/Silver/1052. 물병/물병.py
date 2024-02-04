b03327be8332b8882919b2e250051671d6596956
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-02-04 10:16:25 +0900

    [Silver I] Title: 물병, Time: 120 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1052. 물병/물병.py
+++ 백준/Silver/1052. 물병/물병.py
@@ -1,17 +1,25 @@
-# 1052 물병
+# 1052
 
-# 입력
-N, K = map(int, input().split())
+n, k = map(int, input().split())
 
-# 2진수로 바꾼 현재 물병의 수(2진수 1의 수)
-water_bottle = bin(N).count('1')
+# 1의 수 세기
+count_ones = bin(n).count('1')
 
-new = N  # 이후 물병의 총 수(10진수)
-cnt = 0  # 추가할 물병의 총 수(10진수)
-bottle = 0  # 추가할 물병
-while bin(new).count('1') > K:
-    bottle = 2 ** bin(new)[::-1].index('1')
-    new += bottle
-    cnt += bottle
+# 줄여야 할 물병의 수
+bottles_to_reduce = count_ones - k
 
-print(cnt)
+if bottles_to_reduce <= 0:
+    print(0)
+
+else:
+    answer =0
+    power= 0
+    while bottles_to_reduce > 0:
+        # 계속 1이 있으면 그만큼 더해서 bottle_to_reduce 가 0보다 작거나 같아질때가지 만듦.
+        if n & (1<<power):
+            n += (1<<power)
+            answer+= (1<<power)
+            bottles_to_reduce = bin(n).count('1') - k
+        power += 1
+
+    print(answer)
