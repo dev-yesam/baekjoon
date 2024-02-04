de2c94f5c10c8048bc718c7c5d0e770e23634739
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-02-04 12:03:59 +0900

    [Silver III] Title: 거북이, Time: 812 ms, Memory: 110840 KB -BaekjoonHub

--- 백준/Silver/8911. 거북이/거북이.py
+++ 백준/Silver/8911. 거북이/거북이.py
@@ -1,41 +1,36 @@
-# 8911
+# 거북이
+
+# 움직임
+moves = {"F": 1, "B": -1}
+rotations = {"L": -1, "R": 1}
+
+# 우회전 기준 : 상 우 하 좌
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+d = 0
+
 
 # 입력
 t = int(input())
-for tc in range(t):
-    s = input()
+for _ in range(t):
 
-    # 델타 상 우 하 좌
-    dr = [-1, 0, 1, 0]
-    dc = [0, 1, 0, -1]
+    # 현 위치
+    x, y = 0, 0
 
-    # 이동과 방향
-    move = {'F': 1, 'B': -1}
-    directions = {"L": -1, "R": 1}
+    Mx, mx, My, my = 0, 0, 0, 0
 
-    # 최대 좌표
-    M_x, m_x = 0, 0
-    M_y, m_y = 0, 0
+    directions = input()
 
-    # 방향
-    d = 0  # 기본값 상
+    for direction in directions:
+        if direction in moves:
+            x += dx[d] * moves[direction]
+            y += dy[d] * moves[direction]
 
-    # 위치
-    r, c = 0, 0
+        elif direction in rotations:
+            d = (d + rotations[direction]) % 4
 
-    # 이동
-    for i in s:
-        if i in move:
-            r = r + dr[d] * move[i]
-            c = c + dc[d] * move[i]
+        Mx, My = max(Mx, x), max(My, y)
+        mx, my = min(mx, x), min(my, y)
 
-        elif i in directions:
-            d = (d + directions[i]) % 4
-
-        # 최대 최소 좌표 확인
-        M_x, m_x = max(M_x, r), min(m_x,r)
-        M_y, m_y = max(M_y, c), min(m_y,c)
-    
-    # 최대 직사각형 넓이 계산 및 출력
-    area = (M_x - m_x) * (M_y - m_y)
+    area = (Mx - mx) * (My - my)
     print(area)
