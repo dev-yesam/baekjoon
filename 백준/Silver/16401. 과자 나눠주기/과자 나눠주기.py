41ec653a4f5049ec67895839b5bc26fcb8417a03
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-02-05 19:31:01 +0900

    [Silver II] Title: 과자 나눠주기, Time: 828 ms, Memory: 254052 KB -BaekjoonHub

---
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.py
@@ -0,0 +1,29 @@
+# 입력
+
+kids_num, snacks_num = map(int, input().split())
+snacks = list(map(int, input().split()))
+
+start = 1
+end = max(snacks)
+
+res = 0  # 기본값
+
+while start <= end:
+
+    # 자른 막대과자
+    cut_snacks = 0
+
+    # 자르는 지점 middle
+    cut = (start + end) // 2
+
+    for snack in snacks:
+        cut_snacks += (snack // cut)
+        
+    if cut_snacks >= kids_num:
+        start = cut +1
+        res = cut
+    
+    else:
+        end = cut - 1
+
+print(res)
