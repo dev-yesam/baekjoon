341e7b96283d47f7ef71f8508439ee2d87277fd4
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-02-07 11:14:55 +0900

    [Silver III] Title: 눈 치우기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/26215. 눈 치우기/눈 치우기.py
+++ 백준/Silver/26215. 눈 치우기/눈 치우기.py
@@ -6,23 +6,20 @@ house_num = int(input())
 snows = list(map(int, input().split()))
 snows.sort(reverse=True)
 cnt = 0
-while True:
-    if len(snows) == 1:
-        cnt = snows[0]
-        break
-    else:
-        if snows[0] == 0:
-            break
-        else:
-            snows[0] -= 1
 
-        if snows[1] == 0:
-            pass
-        else:
+while snows[0] > 0:
+    cnt += 1
+
+    if house_num >= 2:
+        snows[0] -= 1
+
+        if snows[1] > 0:
             snows[1] -= 1
 
-        cnt += 1
         snows.sort(reverse=True)
+    else:
+        cnt = snows[0]
+        break
 
 if cnt <= 1440:
     print(cnt)
