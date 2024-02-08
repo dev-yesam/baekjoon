bfe756203d17af7a8ee1ad2d549d9473697ae745
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-02-09 00:48:36 +0900

    [Silver II] Title: 스택 수열, Time: 176 ms, Memory: 117320 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,28 +1,43 @@
 import sys
-
 input = sys.stdin.readline
-ans =[]
+
 n = int(input())
-stack = []
+stack =[0]
+lst = []
+
+
+num = 1
 answer = True
-start= 1
 for _ in range(n):
-    num = int(input())
+    x = int(input())
+    # 가능하면 +, - 로 출력
 
-    while start <= num:
-        stack.append(start)
-        ans.append('+')
-        start += 1
 
-    if stack[-1] == num:
-        stack.pop()
-        ans.append('-')
-    else:
-        answer = False
 
-if not answer:
-    print('NO')
+    while num < x:
+        # x 보다 작을 때
+        stack.append(num)
+        lst.append('+')
+        num += 1
+
+
+    # x 와 같을 때
+    if num == x:
+        lst.append('+')
+        lst.append('-')
+        num +=1
+
+    # x 보다 클 때
+    elif num > x:
+        if stack[-1] == x:
+            lst.append('-')
+            stack.pop()
+        else:
+            answer = False
+            break
+
+if answer:
+    for i in lst:
+        print(i)
 else:
-    for l in ans:
-        print(l)
-
+    print("NO")
