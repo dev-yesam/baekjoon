3fae1893beae39a924bd8690b2ece4412bcd92c2
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-02-13 00:00:59 +0900

    [Silver III] Title: 햄버거 분배, Time: 72 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/19941. 햄버거 분배/햄버거 분배.py
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.py
@@ -1,23 +1,11 @@
-# 19941 재 재출
-import sys
-
-n, k = map(int,sys.stdin.readline().split())
-locations = list(sys.stdin.readline().rstrip())
-# 우측으로 탐색하며 가장 왼쪽에 있는 햄버거 먹기
-
-#카운트
+n, k = map(int, input().split())
+s = list(input())
 cnt = 0
-
-# 반복문 돌리며, 탐색
-for idx in range(n):
-    if locations[idx] == 'P':
-
-        # i위치에서 -k~ k 까지 탐색하며, 가장 왼쪽 햄버거 퍼먹기 
-        for i in range(max(idx-k,0), min(idx+k+1, n)):
-            if locations[i]=='H':
-                locations[i]=0
+for i in range(len(s)):
+    if s[i] == 'P':
+        for j in range(max(0, i-k), min( i+k+1    ,len(s))):
+            if s[j] == 'H':
                 cnt+=1
+                s[j]= 0
                 break
-
-# 출력
 print(cnt)
