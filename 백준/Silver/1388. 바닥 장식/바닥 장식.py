d52abb4a7721aae1565625655dd123cafcead53b
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-02-13 14:29:35 +0900

    [Silver IV] Title: 바닥 장식, Time: 124 ms, Memory: 111744 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.py
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.py
@@ -1,28 +1,20 @@
-import sys
-input = sys.stdin.readline
-# 입력
+# n 세로 길이, m 가로 길이
 n, m = map(int, input().split())
+graph = [list(input()) for _ in range(n)]
 
-# 하나 하나 보면서, 맞으면 0으로 바꿔버려도 될듯.
-
-graph = []
-for _ in range(n):
-    graph.append(list(input().rstrip()))
-# print(graph)
+visited = [[0] *m for _ in range(n)]
 
 
-# 방문 리스트
-visited = [ [0] * m for _ in range(n)]
-
-# '-' 는 오른쪽 dfs , '|'는 아래쪽 dfs
 def dfs(r,c):
-    visited[r][c]= 1
-    if graph[r][c]== '-':
-        if c+1 < m and graph[r][c+1] == '-' and visited[r][c+1] == 0:
-            dfs(r, c+1)
-    elif graph[r][c] =='|':
+
+    visited[r][c] = 1
+
+    if graph[r][c] == '-':
+        if c+1 < m and graph[r][c+1] == '-' and visited[r][c+1] ==0:
+            dfs(r,c+1)
+    elif graph[r][c] == '|':
         if r+1 < n and graph[r+1][c] == '|' and visited[r+1][c] == 0:
-            dfs(r+1, c)
+            dfs(r+1,c)
 
 # 개수 세기
 cnt = 0
@@ -30,7 +22,6 @@ for i in range(n):
     for j in range(m):
         if visited[i][j] == 0:
             cnt += 1
-            dfs(i,j)
+            dfs(i, j)
 
 print(cnt)
-
