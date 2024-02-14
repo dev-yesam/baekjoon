37e867f9b3c7a545975a6c6d3feb090e148f97cf
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-02-14 13:50:57 +0900

    [Silver IV] Title: 점프왕 쩰리 (Small), Time: 108 ms, Memory: 236084 KB -BaekjoonHub

--- 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.py
+++ 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.py
@@ -1,5 +1,6 @@
 import sys
-sys.setrecursionlimit(10**5)
+
+sys.setrecursionlimit(10 ** 5)
 n = int(input())
 
 map = [list(map(int, input().split())) for _ in range(n)]
@@ -14,30 +15,21 @@ def dfs(map, r, c):
     # 현재 위치 바꿀 필요 없음.
     # visited[r][c]= 1
 
-    #
-    global answer
-    # if answer_right or answer_down :
-    if map[r][c] == -1:
-        answer = True
-        return
-    
-    if map[r][c] == 0:
-        return
+    # 현재 방문처리
+    visited[r][c] = 1
 
     # 이동량
     m = map[r][c]
-    # 우측 이동
-    if r + m < n:
+
+    # 다음 위치 방문
+    if r + m < n and visited[r + m][c] == 0:
         dfs(map, r + m, c)
-    # 하측 이동
-    if c + m < n:
+    if c + m < n and visited[r][c + m] == 0:
         dfs(map, r, c + m)
 
 
-
-
 dfs(map, 0, 0)
-if answer:
+if visited[-1][-1] == 1:
     print('HaruHaru')
 else:
     print('Hing')
