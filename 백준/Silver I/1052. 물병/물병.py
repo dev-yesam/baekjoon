213c1a1f6851b78e0025b2079b54c89c323b896f
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-02-16 10:20:58 +0900

    [Silver I] Title: 물병, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver I/1052. 물병/물병.py
+++ 백준/Silver I/1052. 물병/물병.py
@@ -1,25 +1,25 @@
-# 1052
+# 입력
+bottles, k = map(int, input().split())
 
-n, k = map(int, input().split())
+# 현재 물병 수 : 2진수로 바꿨을 때, 1의 개수
+bi_bottles = bin(bottles)[2:]
+now_bottles = bi_bottles.count('1')
 
-# 1의 수 세기
-count_ones = bin(n).count('1')
-
-# 줄여야 할 물병의 수
-bottles_to_reduce = count_ones - k
-
-if bottles_to_reduce <= 0:
+if now_bottles <= k:
     print(0)
+    exit()
 
-else:
-    answer =0
-    power= 0
-    while bottles_to_reduce > 0:
-        # 계속 1이 있으면 그만큼 더해서 bottle_to_reduce 가 0보다 작거나 같아질때가지 만듦.
-        if n & (1<<power):
-            n += (1<<power)
-            answer+= (1<<power)
-            bottles_to_reduce = bin(n).count('1') - k
-        power += 1
+# 13 : 8 4 1 => 1101 => 4- '01'
+# 2진법 앞에서 k번째 1에서 남은 애들 빼준만큼 더해주면 되네.
 
-    print(answer)
+cnt = 0
+idx_n = 0
+for idx in range(len(bi_bottles)):
+    if bi_bottles[idx] == '1':
+        cnt += 1
+        if cnt == k:
+            idx_n = idx
+
+answer = int(bi_bottles[idx_n:], 2) - 2 * int(bi_bottles[idx_n + 1:], 2)
+
+print(answer)
