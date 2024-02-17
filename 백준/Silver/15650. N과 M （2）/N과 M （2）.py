b6acde88203307e096478c641a11667dcd445c21
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-02-17 17:55:52 +0900

    [Silver III] Title: N과 M (2), Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -5,24 +5,22 @@ res = []
 visited = [0] * (n + 1)
 
 
-def recur(num):
-    # 1. 가지치기
-    if res != sorted(res):
-        return
+def recur(num,start):
+
     # 2. 종료조건
     if num == m:
         print(*res)
         return
 
     # 3. 재귀호출
-    for i in range(1, n + 1):
+    for i in range(start, n + 1):
         if not visited[i]:
             visited[i] = 1
             res.append(i)
-            recur(num + 1)
+            recur(num + 1,i+1)
 
             # 복구
             res.pop()
             visited[i] = 0
 
-recur(0)
+recur(0,1)
