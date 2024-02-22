90e6e40c3339ae0609b73e62848a43d8727a6c90
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-02-22 14:49:10 +0900

    [Silver III] Title: 거북이, Time: 728 ms, Memory: 112416 KB -BaekjoonHub

--- 백준/Silver/8911. 거북이/거북이.py
+++ 백준/Silver/8911. 거북이/거북이.py
@@ -1,36 +1,39 @@
-# 거북이
-
-# 움직임
-moves = {"F": 1, "B": -1}
-rotations = {"L": -1, "R": 1}
-
-# 우회전 기준 : 상 우 하 좌
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
-d = 0
+# x좌표 최대 최소의 차 * y 좌표의 최대 최소의 차
+# 델타 배열
 
 
-# 입력
 t = int(input())
 for _ in range(t):
+    cmd = list(input())
 
-    # 현 위치
-    x, y = 0, 0
+    x, y = 0, 0  # 현위치
+    dx = [0, 1, 0, -1]
+    dy = [1, 0, -1, 0]
 
-    Mx, mx, My, my = 0, 0, 0, 0
+    d = 0
+    Mx, mx = 0, 0
+    My, my = 0, 0
 
-    directions = input()
+    # 명령어 실행
+    for c in cmd:
 
-    for direction in directions:
-        if direction in moves:
-            x += dx[d] * moves[direction]
-            y += dy[d] * moves[direction]
+        if c == 'F':
+            x += dx[d]
+            y += dy[d]
+            Mx, mx = max(Mx, x), min(mx, x)
+            My, my = max(My, y), min(my, y)
 
-        elif direction in rotations:
-            d = (d + rotations[direction]) % 4
+        elif c == 'B':
+            x -= dx[d]
+            y -= dy[d]
+            Mx, mx = max(Mx, x), min(mx, x)
+            My, my = max(My, y), min(my, y)
 
-        Mx, My = max(Mx, x), max(My, y)
-        mx, my = min(mx, x), min(my, y)
+        elif c == 'L':
+            d = (d - 1) % 4
 
-    area = (Mx - mx) * (My - my)
-    print(area)
+        elif c == 'R':
+            d = (d + 1) % 4
+
+
+    print((Mx-mx)*(My-my))
