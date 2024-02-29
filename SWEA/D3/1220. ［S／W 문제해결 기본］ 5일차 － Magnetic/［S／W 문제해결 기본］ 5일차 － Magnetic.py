c67f86e6454743e1f51e0b09bafbcdd92e34a256
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-02-29 14:33:01 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 198 ms, Memory: 60,024 KB -BaekjoonHub

--- SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
@@ -1,19 +1,34 @@
-# 위에서부터 빨간색을 만나면 prev에 넣고.
-# prev 밑으로 내려가면서 파란색 만나면 cnt += 1
+def ver_cnt(col):
+    cnt = 0
+    prev = False
+    for row in range(n):
+        # red가 나오면?
+        if arr[row][col] == 1:
+            prev = True
+
+        # blue가 나오고 이전에 red가 있었다면?
+        elif arr[row][col] == 2 and prev:
+            cnt += 1
+            prev = False
+
+    return cnt
+
 
 t = 10
 for tc in range(1, 11):
-    n = int(input())  # 어차피 100
+
+    n = int(input())  # 100
     arr = [list(map(int, input().split())) for _ in range(n)]
 
-    cnt = 0
-    for j in range(100):
-        prev = 0
-        for i in range(100):
-            if arr[i][j] == 1:
-                prev = 1
-            elif arr[i][j] == 2 and prev == 1:
-                cnt += 1
-                prev = 2
+    total_cnt = 0
+    # 1열~n열 돌아가면서
+    for col in range(n):
+        total_cnt += ver_cnt(col)
 
-    print(f'#{tc}', cnt)
+    ### 함수 처리
+    # 각 열에서 red 있다면
+    # 뒤에 blue가 나오는지
+    # 그리고 또 뒤에 red가 나오면
+    # 또 뒤에 blue 가 있는지.
+
+    print(f'#{tc} {total_cnt}')
