1851ba8f2e58ac709eca842c13300978e692f8e8
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-02-29 12:11:04 +0900

    [D3] Title: 오목 판정, Time: 205 ms, Memory: 60,276 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.py
+++ SWEA/D3/11315. 오목 판정/오목 판정.py
@@ -1,41 +1,41 @@
-def omok(r, c):
-    dr = [1, 0, 1, 1]  # |하, -우, \우하, /좌하
-    dc = [0, 1, 1, -1]  # |하, -우, \우하, /좌하
+def f(r, c):
+    # 우 하 우하 좌하 - | \ /  탐색
+    dr = [0, 1, 1, 1]
+    dc = [1, 0, 1, -1]
 
-    # 4방향 탐색
     for d in range(4):
-        # 기준 좌표에 돌이 있음
         cnt = 1
-        # 돌 4개를 탐색
-        for power in range(1, 5):
-            nr = r + (dr[d] * power)
-            nc = c + (dc[d] * power)
+        # 거리 차이
+        for m in range(1, 5):
+
+            nr = r + dr[d]*m
+            nc = c + dc[d]*m
+            # 범위를 벗어난다면
             if not (0 <= nr < n and 0 <= nc < n):
                 break
-
-            # 돌을 발견하면 cnt
+            # 돌이 있다면 카운트 증가
             if arr[nr][nc] == 'o':
                 cnt += 1
-
-            # 오목이면
+            # 오목이면 True 반환
             if cnt == 5:
                 return True
 
     return False
 
-
-def game_start():
+def game():
     for i in range(n):
         for j in range(n):
             if arr[i][j] == 'o':
-                if omok(i, j):
-                    return "YES"
+                if f(i, j):
+                    return 'YES'
     return "NO"
-
 
 t = int(input())
 for tc in range(1, t + 1):
+
     n = int(input())
     arr = [list(input()) for _ in range(n)]
-    ans = game_start()
+
+    ans = game()
+
     print(f'#{tc}', ans)
