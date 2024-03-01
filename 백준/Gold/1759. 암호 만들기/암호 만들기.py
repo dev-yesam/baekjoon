e6e4e4377f581dc331ba7b9869775d3a830e3d40
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-03-01 12:32:44 +0900

    [Gold V] Title: 암호 만들기, Time: 168 ms, Memory: 113560 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,55 +1,70 @@
-# L 암호 수
-# C 가능성 있는 문자 수
-# aeiou 1개 이상, 자음 2개 이상
+# L 개의 알파벳으로 암호가 구성됨
+# 최소 1개의 모음(aeiou)과 최소 2개의 자음
+# 암호 알파벳 오름차순
+# C 개의 가능한 암호 수
+
+# 모음 자음 리스트에서 n 개 뽑는 함수
+# 백트래킹
+def cmb(level, start, cnd_lst, vclst, n):
+    if level == n:
+        cnd_lst.append(cmb_lst[:])
+        return
+
+    # 재귀 호출
+    for i in range(start, len(vclst)):
+        cmb_lst.append(vclst[i])
+        cmb(level+1, i+1, cnd_lst, vclst,n)
+        cmb_lst.pop()
+
+
+
+
+# 입력
 L, C = map(int, input().split())
-lst = list(input().split())
-vowels_lst = ['a', 'e', 'i', 'o', 'u']
-vow = []
-con = []
-for char in lst:
-    if char in vowels_lst:
-        vow.append(char)
+
+# C 개 중에서,
+# 모음 1개 , 자음 L-1 개
+# 모음 2개 , 자음 L-2 개
+# ...
+# 모음 L-2개, 자음 2개
+
+# C 입력 받아 모음 자음 분할
+vowel_lst = ['a', 'e', 'i', 'o', 'u']
+lst = input().split()
+vowels = []
+cons = []
+for character in lst:
+    if character in vowel_lst:
+        vowels.append(character)
     else:
-        con.append(char)
-
-final = []
-for vow_num in range(1, L - 1):
-    con_num = L - vow_num
+        cons.append(character)
 
 
-    # 조합 함수
-    def comb(level, goal, start, lst, password, num, vow_con):
-        if level == goal:
-            password.append(lst[:])
-            return
+# 모음 개수별로 조합으로 뽑은 다음 오름차순 돌리기
+total = []
+for q in range(1, min(L - 1, len(vowels)+1)):
 
-        for i in range(start, num):
-            lst.append(vow_con[i])
-            comb(level + 1, goal, i + 1, lst, password, num, vow_con)
-            lst.pop()
+    # 뽑은 모음
+    vow_cnd =[] # 모음 후보군
+    cmb_lst = []
+    cmb(0, 0,vow_cnd, vowels, q)
 
 
-    ## 모음에서 vow_num 개 뽑고,
-    vow_pass = []
-    temp1 = []
-    comb(0, vow_num, 0, temp1, vow_pass, len(vow), vow)
-    ## 자음에서 con_num 개 뽑고,
-    con_pass = []
-    temp2 = []
-    comb(0, con_num, 0, temp2, con_pass, len(con), con)
+    # 뽑은 자음
+    con_cnd = [] # 자음 후보군
+    cmb_lst = []
+    cmb(0, 0, con_cnd, cons, L-q)
 
-    ## 두개 for문으로 곱해준 후에 정렬하고 출력
-    # 최종
+    # 자음 모음 후보군 각각 마다 출력
 
-    for i in vow_pass:
-        for j in con_pass:
-            password = []
-            password.extend(i)
-            password.extend(j)
-            password.sort()
-            final.append(''.join(password))
+    for vow in vow_cnd:
+        for con in con_cnd:
+            n = vow+con
+            n.sort()
+            n = ''.join(n)
+            total.append(n)
 
-# 출력
-final.sort()
-for i in final:
-    print(i)
+total.sort()
+for t in total:
+    print(t)
+
