c050122aa8c2f3bfe1ce6eb76042a1aa42f91828
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-03-02 20:43:52 +0900

    [Silver I] Title: 숨바꼭질, Time: 152 ms, Memory: 114780 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+N, target = map(int, input().split())
+
+# N < target
+# [1] while 2*N < target:
+# [2] N +1...
+if N >= target:
+    cnt = N - target
+    print(cnt)
+
+# N > target
+# N -1....
+else:
+    def bfs():
+        # 시작점
+        # [1] 사전 방문 처리
+        visited = [0] * (10 ** 5 + 1)
+        visited[N] = 1
+
+        # [2] 방문 행동 -> 없음
+        # [3] 방문 예약
+        q = deque()
+        q.append(N)
+
+        # 방문 예약 처리
+        while q:
+            now = q.popleft()
+            if now == target:
+                return visited[now] - 1
+
+            for next in (now - 1, now + 1, now * 2):
+                if 0 <= next <= 10 ** 5 and visited[next] == 0:
+                    visited[next] = visited[now] + 1
+                    q.append(next)
+
+
+    cnt = bfs()
+    print(cnt)
