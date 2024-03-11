8d3cb7b3e2f00308b48ccbc14abd0757d7ea3f90
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-03-11 21:45:40 +0900

    [Gold V] Title: 트리, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.py
+++ 백준/Gold/1068. 트리/트리.py
@@ -1,74 +1,36 @@
-# 입력
-from collections import deque
+### 자식 인덱스를 기준으로 부모 노드 정보 저장하는 arr 에서
+## 리프노드를 확인하는 방법 익혀두기
+
 
 n = int(input())
 
 # 자식 인덱스로 부모 저장
 arr = list(map(int, input().split()))
 
-# 지울 번호 리스트
+# 지울 번호
 x = int(input())
-lst = []
-lst.append(x)
 
+# dfs로 삭제
 # 지운 번호는 -2 두자
-arr[x] = -2
-for i in range(x + 1, n):
-    if arr[i] in lst:
-        lst.append(i)
-        arr[i] = -2
-
-
-# 부모 인덱스로 자식 저장
-tree= [[] for _ in range(n)]
-for i in range(n):
-    if arr[i] == -2:
-        pass
-    elif arr[i] == -1:
-        root = i
-    else:
-        tree[arr[i]].append(i)
-
-cnt =0
-visited = [0] *n
-def dfs(root):
-    global cnt
-    visited[root] =1
-    # 1. 종료조건
-    if not tree[root]:
-        cnt +=1
-        return
-    # 2. 가지치기
-
-    # 3. 재귀호출
-    for i in tree[root]:
-        if not visited[i] and arr[i] != -2:
+def dfs(x):
+    arr[x] = -2
+    for i in range(n):
+        if arr[i] ==x:
             dfs(i)
+dfs(x)
 
-if -1 in arr:
-    dfs(root)
-    print(cnt)
-else:
-    print(0)
+# 카운트
+cnt =0
+
+### 리프노드는 이렇게 하면 되겠다.
+## 근데 시간이 빠른지는 나중에 생각하면 될듯.
+# 리프 노드 확인
+for i in range(n):
+    # 삭제되지 않았으면서, 누군가의 부모가 아니다.
+    if arr[i] != -2 and i not in arr:
+        cnt += 1
+
+# 출력
+print(cnt)
 
 
-
-
-
-
-
-
-
-
-# # 자식있는 번호 리스트
-# lst2= []
-# cnt = 0
-# for i in range(n):
-#     # 부모가 있다면
-#     if arr[i] != -2:
-#         if i not in lst2:
-#             lst.append(arr[i])
-#             lst.append(i)
-#             cnt += 1
-#         else:
-#             lst2.append(arr[i])
