b0874902bbad4e3dff715c9a98a2475ec762e220
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-03-20 21:52:13 +0900

    [Silver II] Title: 이동하기, Time: 252 ms, Memory: 127812 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -1,21 +1,16 @@
-# 시작 위치 (1,1)
-# 이동 하, 우, 우하 3가지만
-# 많은 칸 수 지나가려면?
-
+## 패딩 준 버젼. 인덱스 덜 헷갈리긴 하는 듯.
 
 n, m = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
+arr = [[0]*(m+1)] + [[0]+list(map(int, input().split())) for _ in range(n)]
+
 
 # d[i][j] = d[i-1][j] + d[i][j-1]
-# 디피 테이블 (패딩 + 1)
-d = [[0] * (m + 1) for _ in range(n)]
-# 초기값
-d[0] = arr[0] + [0]
-for i in range(m):
-    d[0][i] = d[0][i] + d[0][i - 1]
+# 디피 테이블 (좌, 상 패딩 + 1)
+d = [[0] * (m + 1) for _ in range(n+1)]
+# 초기값 생략
 
-for i in range(1, n):
-    for j in range(m):
+for i in range(1, n+1):
+    for j in range(1, m+1):
         d[i][j] = max(d[i - 1][j], d[i][j - 1], d[i - 1][j - 1]) + arr[i][j]
 
-print(d[n - 1][m - 1])
+print(d[n][m])
