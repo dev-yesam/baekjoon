76fc1f2543837c2b6e1b0f0b4c72070b3dd5a449
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-03-21 19:39:52 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 372 ms, Memory: 125668 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,43 +1,51 @@
-import heapq
+# 크루스칼 알고리즘
+# disjoint-set
+def find_set(x):
+    if x == p[x]:
+        return x
+    else:
+        p[x] = find_set(p[x])
+        return p[x]
+
+def union_set(x,y):
+    x = find_set(x)
+    y = find_set(y)
+
+    if x==y:
+        return # 아무것도 안함. 근데 이미 다른 것만 함.
+
+    # 연결해야 한다면
+    if x < y:
+        p[y] = x
+    else:
+        p[x] = y
 
 
-def prim(start):
-    visited = [0] * (v+1)
-    heap = []
-    sum_weight = 0
+# 크루스칼 사용
 
-    # 시작점 처리
-    heapq.heappush(heap, (0, start))
-
-    # 방문 예약
-    while heap:
-        # 꺼내기
-        weight, now = heapq.heappop(heap)
-
-        if visited[now]:
-            continue
-
-        # 밑에서 미리 방문 처리하면 안되는 구나. 여러 후보군 중에서 결국 최소힙으로 나오는 걸 골라야하니까.
-        visited[now] = 1
-        sum_weight += weight
-
-        for next_weight, next_node in adjl[now]:
-            if not visited[next_node]:
-                heapq.heappush(heap, (next_weight, next_node))
-
-    return sum_weight
-
-
-# 그래프 정보 입력
 v, e = map(int, input().split())
-adjl = [[] for _ in range(v+1)]
+edges = []
 for _ in range(e):
-    s, e, w = map(int, input().split())
-    adjl[s].append((w, e))
-    adjl[e].append((w, s))
+    start, end, weight = map(int, input().split())
+    edges.append((start, end, weight))
 
+sum_weight = 0
 
+# edges 오름차순 정렬
+edges.sort(key=lambda x: x[2])
 
-# prim 알고리즘
-ans = prim(1)
-print(ans)
+# disjoint set 준비
+p = [i for i in range(v + 1)]
+
+# 간선들 확인
+for start, end, weight in edges:
+
+    # start와 end 가 연결되어있다? => 뭐 안하면 됨
+    if find_set(start) == find_set(end):
+        continue
+
+    # 연결 안되어 있다? => 최소 신장 트리 확장
+    union_set(start, end)
+    sum_weight += weight
+
+print(sum_weight)
