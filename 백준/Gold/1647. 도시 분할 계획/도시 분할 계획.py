01cb8fa9c2606dbbcefc8c06b5338127a05d0c45
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-03-22 19:11:47 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2260 ms, Memory: 292036 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -0,0 +1,48 @@
+def find_set(x):
+    if x == p[x]:
+        return x
+
+    else:
+        p[x] = find_set(p[x])
+        return p[x]
+
+
+def union(x, y):
+    x = find_set(x)
+    y = find_set(y)
+
+    if x == y:
+        return
+
+    if x < y:
+        p[y] = x
+    else:
+        p[x] = y
+
+
+def kruskal():
+    ans = 0
+    cnt = 0
+    for s, e, w in edges:
+        if find_set(s) != find_set(e):
+            union(s, e)
+            ans += w
+            cnt += 1
+            if cnt == n - 2:
+                return ans
+    return 0
+
+n, m = map(int, input().split())
+edges = []
+p = [i for i in range(n + 1)]
+for _ in range(m):
+    s, e, w = map(int, input().split())
+    edges.append((s, e, w))
+    # mst 만든 다음에 제일 가중치 높은 간선 없애면 되겠네.
+
+edges.sort(key=lambda x: x[2])
+
+# 크루스칼 써야할 듯.
+ans = kruskal()
+
+print(ans)
