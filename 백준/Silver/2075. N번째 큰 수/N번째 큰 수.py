68ccc213ae80d5e350eea14282b9ea3523da6edb
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-03-23 17:07:36 +0900

    [Silver II] Title: N번째 큰 수, Time: 644 ms, Memory: 115948 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
@@ -1,17 +1,17 @@
-from heapq import heappush, heappop
+##### 테스트  제출용 #########
 
+import heapq
+
+heap = []
 n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
 
-# n번째 큰 수는
-nums = []
-for i in range(n):
-    for j in range(n):
-        heappush(nums, arr[i][j])
-
-        if len(nums) > n:
-            heappop(nums)
-
-
-print(heappop(nums))
-
+for _ in range(n):
+    numbers = map(int, input().split())
+    for number in numbers:
+        if len(heap) < n: # heap의 크기를 n개로 유지
+            heapq.heappush(heap, number)
+        else:
+            if heap[0] < number:
+                heapq.heappop(heap)
+                heapq.heappush(heap, number)
+print(heap[0])
