0daea759daf88b3dff484353bd7333a5086c5237
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-03-23 20:00:33 +0900

    [Gold V] Title: 두 용액, Time: 144 ms, Memory: 124064 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,33 @@
+n = int(input())
+lst = list(map(int, input().split()))
+lst.sort()
+# 용액들 오름차순 정렬 되어 있음
+
+
+start = 0
+end = n - 1
+
+cnt = 0
+ans = abs(lst[start] + lst[end])
+ans_start = start
+ans_end = end
+
+# 투 포인터 돌리기
+while start < end:
+    total = lst[start] + lst[end]
+    # 갱신
+    if abs(total) < ans:
+        ans_start = start
+        ans_end = end
+        ans = abs(total)
+
+        if ans == 0:
+            break
+
+    ## 토탈이 음수냐 양수냐가 중요했던 문제
+    if total < 0:
+        start += 1
+    else:
+        end -= 1
+
+print(lst[ans_start], lst[ans_end])
