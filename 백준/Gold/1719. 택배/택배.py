c90d7bcc240282b30ec29891b4216cd6ba03f556
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-03-24 11:52:59 +0900

    [Gold III] Title: 택배, Time: 312 ms, Memory: 114552 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.py
+++ 백준/Gold/1719. 택배/택배.py
@@ -1,50 +1,57 @@
 from heapq import heappush, heappop
+import sys
+
+input = sys.stdin.readline
 
 
 def dijkstra(start):
+    # 시작점
+    tr[start][start] = '-'
+    dists = [INF] * (n + 1)
+    dists[start] =0
     pq = []
     heappush(pq, (0, start))
-    dists[start][start] = 0
 
+    # 다음 방문 예약
     while pq:
         # 꺼내기
-        now_dist, now = heappop(pq)
+        dist, now = heappop(pq)
 
-        if now_dist > dists[start][now]:
+        # 꺼냈는데 전보다 작다?
+        if dists[now] < dist:
             continue
 
-        # 다음 노드
+        # 다음 방문
         for next_weight, next in adjl[now]:
-            next_dist = next_weight + now_dist
-            if next_dist < dists[start][next]:
-                dists[start][next] = next_dist
+            next_dist = dist + next_weight
+            if next_dist < dists[next]:
+                dists[next] = next_dist
                 heappush(pq, (next_dist, next))
 
-                # 경로 추가 (이전 경로 + 현재 위치)
-                tr[start][next] = tr[start][now] + [next]
+                # 시작점에서 바로 갔다면?
+                if now == start:
+                    tr[start][next] = next
+                # now가 경유지라면?(그래서 뭐가 now에 들어있다면?)
+                elif tr[start][now]:
+                    tr[start][next] = tr[start][now]
 
 
-# 경로표 => 최단 경로
-# 최단 거리가 아닌 최단 경로가 필요. => 경유지 저장해야함.
 
-n, m = map(int, input().split())
+
+n, m = map(int, input().split())  # 노드의 수, 간선의 수
 adjl = [[] for _ in range(n + 1)]
 for _ in range(m):
-    s, e, w = map(int, input().split())
-    adjl[s].append((w, e))
-    adjl[e].append((w, s))
+    a, b, w = map(int, input().split())
+    adjl[a].append((w, b))
+    adjl[b].append((w, a))
 
-INF = 200000
-dists = [[INF] * (n + 1) for _ in range(n + 1)]
-tr = [[[] for _ in range(n + 1)] for i in range(n + 1)]
+tr = [[0] * (n + 1) for _ in range(n + 1)]  # 경로표
+
+# 다익스트라 호출
+INF = 3000000
 for i in range(1, n + 1):
     dijkstra(i)
 
 # 출력
 for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        if not tr[i][j]:
-            print('-', end=' ')
-        else:
-            print(tr[i][j][0], end=' ')
-    print()
+    print(*tr[i][1:])
