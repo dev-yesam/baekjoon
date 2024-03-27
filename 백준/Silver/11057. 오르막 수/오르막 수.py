276d6597a924eeb9e52cd4b566e01f05104d95a3
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-03-27 19:30:02 +0900

    [Silver I] Title: 오르막 수, Time: 136 ms, Memory: 111140 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.py
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -1,15 +1,8 @@
 n = int(input())
-
-#
-# n자리 일 때 개수 = d[n]
-
-d = [[0] * 10 for _ in range(n + 1)]
-
-d[1] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-
-for i in range(2, n + 1):
+dp = [ [0]*10 for _ in range(n+1)]
+dp[1] = [1] * 10
+for i in range(2,n+1):
     for j in range(10):
-        d[i][j] = sum(d[i-1][j:])
+        dp[i][j] = sum(dp[i-1][j:])
 
-ans = sum(d[n])
-print(ans % 10007)
+print(sum(dp[n])%10007)
