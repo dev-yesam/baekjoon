69e8501969e39b5c3246e5465633fb6237d552e4
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-04-05 18:49:04 +0900

    [Gold III] Title: 소문난 칠공주, Time: 700 ms, Memory: 122204 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
@@ -1,58 +1,68 @@
+## for문을 다 돌기도 전에 return 하면 이꼴이 난다~
+## for문에서 continue, break, return에서 실수가 잦은 듯함
+# 사실 백트래킹에서 새로운 가지에 대한 이해 부족인듯. => 백트 개념 공부하면 될듯.
+
 from collections import deque
 
-
-def bfs(x, y, cmb):
-    visited = [[0] * 5 for _ in range(5)]
-    # 시작점 처리
-    visited[x][y] = 1
-    q = deque()
-    q.append((x, y))
-    cnt = 1  # 7공주 수 세기
-
-    # 다음 방문 예약
-    while q:
-        cx, cy = q.popleft()
-
-        for nx, ny in [(cx - 1, cy), (cx + 1, cy), (cx, cy - 1), (cx, cy + 1)]:
-            if 0 <= nx < 5 and 0 <= ny < 5 and not visited[nx][ny] and cmb[nx][ny]:
-                visited[nx][ny] = 1
-                q.append((nx, ny))
-                cnt += 1
-                if cnt == 7:
-                    return True
-
-    # 연결 안 돼 있으면
-    return False
-
-
-def recur(level, start):
-    global ans
+def recur(level, start, y_num):
+    global cnt
+    # 조합
     if level == 7:
-        cmb = [[0] * 5 for _ in range(5)]
-        s_num = 0
-        for c in temp:
-            cmb[c // 5][c % 5] = 1
-            if arr[c//5][c%5] == "S":
-                s_num += 1
-        if s_num <4:
-            return
-        if bfs(temp[0] // 5, temp[0] % 5, cmb):
-            ans += 1
-            return
+        for i in range(5):
+            for j in range(5):
+                if cmb[i][j] == 1:
+                    if bfs(i, j) == 7:
+                        cnt += 1
+                    return
+        return
 
-    for i in range(start, 25):
-        temp.append(i)
-        recur(level + 1, i + 1)
-        temp.pop()
+    # 다음 조합 원소
+    for next in range(start, 25):
+        # 임도연 파?
+        if arr[next // 5][next % 5] == 'Y':
+            if y_num + 1 >= 4:
+                continue
+            cmb[next // 5][next % 5] = 1
+            recur(level + 1, next + 1, y_num + 1)
+            cmb[next // 5][next % 5] = 0
+        # not 임도연 파?
+        else:
+            cmb[next//5][next%5] = 1
+            recur(level + 1, next + 1, y_num)
+            cmb[next//5][next%5] = 0
 
 
-arr = [list(input()) for _ in range(5)]
-ans = 0  # 경우의 수
-# 1) 25개 중 7 개를 고르는 조합
-# 2) 그 7개가 서로 연결 돼 있는지 판단하는 bfs
+def bfs(r, c):
+    # 시작점 처리
+    seven_num = 1
+    visited = [[0] * 5 for _ in range(5)]
+    visited[r][c] = 1
+    q = deque()
+    q.append((r, c))
+
+    while q:
+        cr, cc = q.popleft()
+
+        for nr, nc in ((cr - 1, cc), (cr + 1, cc), (cr, cc - 1), (cr, cc + 1)):
+            if 0 <= nr < 5 and 0 <= nc < 5 and not visited[nr][nc] and cmb[nr][nc]:
+                visited[nr][nc] = 1
+                seven_num += 1
+                q.append((nr, nc))
+
+    return seven_num
 
 
-# 1) 7 개 고르는 조합 => 백트래킹으로
-temp = []
-recur(0, 0)
-print(ans)
+arr = [input() for _ in range(5)]
+
+cmb = [[0]*5 for _ in range(5)]
+cnt = 0
+
+# cmb = [[0,0,0,0,0],[1,1,1,1,1],[0,0,1,0,0],[0,0,1,0,0],[0,0,0,0,0]]
+# for i in range(5):
+#     for j in range(5):
+#         if cmb[i][j] == 1:
+#             if bfs(i, j) == 7:
+#                 cnt += 1
+recur(0, 0, 0)
+print(cnt)
+
