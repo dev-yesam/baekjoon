c7ca0ddc42c264ba2b20db703b018ae13a38c3a8
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-04-09 19:58:01 +0900

    [Gold III] Title: 내리막 길, Time: 196 ms, Memory: 115504 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,44 +1,39 @@
-# 240407
-# BFS + 최대힙 + DP 로 풀 수 있을 듯.
-
-from collections import deque
+import sys
 from heapq import heappush, heappop
 
-n, m = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
-
-# DP 테이블
-dt = [[0] * m for _ in range(n)]
+input = sys.stdin.readline
 
 
-# BFS
+# BFS 함수 정의
 def bfs(r, c):
     # 시작점
-    dt[0][0] = 1
-    heap = []
-    heappush(heap, (arr[0][0] * -1, r, c)) # 최대힙
+    dp[r][c] = 1
+    heap = []  # 최대힙
+    heappush(heap, (arr[r][c] * -1, r, c))
 
-    # 방문 예약 탐색
+    # 방문예약 탐색
     while heap:
         # 꺼내기
         height, cr, cc = heappop(heap)
-        height *= -1
 
         # 다음 방문
-        for nr, nc in ((cr-1, cc), (cr+1, cc), (cr, cc-1), (cr, cc+1)):
-            # 범위 내고, 높이가 지금보다 낮다면? -> 갈 수 있음
-            if 0<=nr < n and 0<= nc <m and arr[nr][nc] < arr[cr][cc]:
-                # 방문한 적 있는 애라면?
-                if dt[nr][nc]:
-                    dt[nr][nc] += dt[cr][cc]
-                # 방문한 적 없다면?
+        for nr, nc in [(cr - 1, cc), (cr + 1, cc), (cr, cc - 1), (cr, cc + 1)]:
+            # 범위 내 & 높이가 낮
+            if 0 <= nr < n and 0 <= nc < m and arr[nr][nc] < arr[cr][cc]:
+                if dp[nr][nc]: # 방문한 적 있다면?
+                    dp[nr][nc] += dp[cr][cc]
                 else:
-                    dt[nr][nc] = dt[cr][cc]
-                    heappush(heap, (-1*arr[nr][nc], nr, nc))
+                    dp[nr][nc] = dp[cr][cc]
+                    heappush(heap, (arr[nr][nc]* -1, nr, nc))
 
 
 
+
+################
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [[0] * m for _ in range(n)]
 bfs(0, 0)
 
-# 도착점
-print(dt[n - 1][m - 1])
+print(dp[n-1][m-1])
