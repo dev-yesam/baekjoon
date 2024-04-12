b4b6b7e07e7a4aecc980524e06d934bdb9692beb
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-04-12 20:04:38 +0900

    [D3] Title: 오목 판정, Time: 240 ms, Memory: 49,676 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.py
+++ SWEA/D3/11315. 오목 판정/오목 판정.py
@@ -1,41 +1,70 @@
-def f(r, c):
-    # 우 하 우하 좌하 - | \ /  탐색
-    dr = [0, 1, 1, 1]
-    dc = [1, 0, 1, -1]
+def dfs(r, c, direction, cnt):
+    global ans
+    if cnt >= 5:
+        ans = True
+        return
+    # 우측
+    if direction == 'hor':
+        hor_visited[r][c] = 1
+        if c + 1 < n and arr[r][c+1]=='o':
+            dfs(r, c + 1, 'hor', cnt + 1)
+    # 하측
+    elif direction == 'ver':
+        ver_visited[r][c] = 1
+        if r + 1 < n and arr[r+1][c]=='o':
+            dfs(r + 1, c, 'ver', cnt + 1)
 
-    for d in range(4):
-        cnt = 1
-        # 거리 차이
-        for m in range(1, 5):
+    # 좌하 대각선
+    elif direction == 'adiag':
+        adiag_visited[r][c] = 1
+        if r + 1 < n and c - 1 >= 0 and arr[r+1][c-1]=='o':
+            dfs(r + 1, c - 1, 'adiag', cnt + 1)
 
-            nr = r + dr[d]*m
-            nc = c + dc[d]*m
-            # 범위를 벗어난다면
-            if not (0 <= nr < n and 0 <= nc < n):
-                break
-            # 돌이 있다면 카운트 증가
-            if arr[nr][nc] == 'o':
-                cnt += 1
-            # 오목이면 True 반환
-            if cnt == 5:
-                return True
+    # 우하 대각선
+    else:
+        ddiag_visited[r][c] = 1
+        if r + 1 < n and c + 1 < n and arr[r+1][c+1]=='o':
+            dfs(r + 1, c + 1, 'ddiag', cnt + 1)
 
-    return False
 
-def game():
+def solve():
+    global ans
+    # 우측, 하측, 우하, 좌하만 확인하면 됨.
+
     for i in range(n):
         for j in range(n):
             if arr[i][j] == 'o':
-                if f(i, j):
-                    return 'YES'
-    return "NO"
+                if not hor_visited[i][j]:
+                    dfs(i, j, 'hor', 1)
+                if not ver_visited[i][j]:
+                    dfs(i, j, 'ver', 1)
+                if not adiag_visited[i][j]:
+                    dfs(i, j, 'adiag', 1)
+                if not ddiag_visited[i][j]:
+                    dfs(i, j, 'ddiag', 1)
+
+                if ans == True:
+                    return True
+
+
+##########################################################
 
 t = int(input())
 for tc in range(1, t + 1):
-
     n = int(input())
-    arr = [list(input()) for _ in range(n)]
+    arr = [input() for _ in range(n)]
 
-    ans = game()
+    hor_visited = [[0] * n for _ in range(n)]
+    ver_visited = [[0] * n for _ in range(n)]
+    adiag_visited = [[0] * n for _ in range(n)]
+    ddiag_visited = [[0] * n for _ in range(n)]
 
-    print(f'#{tc}', ans)
+    ########
+    ans = False
+    temp = solve()
+    if temp:
+        result = 'YES'
+    else:
+        result = 'NO'
+
+    print(f"#{tc}", result)
