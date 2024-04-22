79ad4baefb35769cf5c7533e8689dea1788eb8e0
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-04-22 21:48:28 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 119 ms, Memory: 47,648 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
@@ -3,14 +3,14 @@ for tc in range(1, t + 1):
     n = int(input())
     ans_bin = 0
     cnt = 1
-    x =((1 << 10) - 1)
+ 
     while True:
         cmp_bin = str(n * cnt)
         for b in cmp_bin:
             ans_bin = ans_bin | (1 << int(b))
-
-        if ans_bin ==x:
+ 
+        if ans_bin & ((1 << 10) - 1) == (1 << 10) - 1:
             break
         cnt += 1
-
+ 
     print(f'#{tc}', cmp_bin)
