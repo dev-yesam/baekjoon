bfd649d5b98d9debaf0c510a669ec1f90946c4dc
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-04-27 20:47:08 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 154 ms, Memory: 45,236 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
@@ -1,16 +1,19 @@
+# 1. 처음 양 숫자에 계속 += 한 뒤에,
+# 2. 각 자리 수마다 가져와서 있는지 없는지 확인
+# 3. 0~9 다 있게 되면 종료. 그때의 양 번호 출력
+
+
 t = int(input())
 for tc in range(1, t + 1):
-    n = int(input())
-    ans_bin = 0
-    cnt = 1
- 
-    while True:
-        cmp_bin = str(n * cnt)
-        for b in cmp_bin:
-            ans_bin = ans_bin | (1 << int(b))
- 
-        if ans_bin & ((1 << 10) - 1) == (1 << 10) - 1:
-            break
-        cnt += 1
- 
-    print(f'#{tc}', cmp_bin)
+    start_n = int(input())
+    now_n = 0
+    cpr = (1 << 10) - 1
+    now_digits = 0
+
+    while now_digits != cpr:
+        now_n += start_n
+        # 각 자리수가 있으면 추가해주는 것
+        for digit in str(now_n):
+            now_digits = now_digits | (1 << int(digit))
+
+    print(f'#{tc}', now_n)
