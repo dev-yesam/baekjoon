7f0cb2acca0ffbbb71aa4761af98e6eb393bdd52
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-06-09 10:44:11 +0900

    [Gold V] Title: 선 긋기, Time: 2772 ms, Memory: 172204 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.py
+++ 백준/Gold/2170. 선 긋기/선 긋기.py
@@ -2,24 +2,25 @@ import sys
 
 input = sys.stdin.readline
 
-
-
 n = int(input())
 lst = [tuple(map(int, input().split())) for _ in range(n)]
 lst.sort()
-length = lst[0][1]-lst[0][0]
-for i in range(1,n):
+length = lst[0][1] - lst[0][0]
+left = lst[0][0]
+right = lst[0][1]
+for i in range(1, n):
     # 겹친다면?
-    if lst[i-1][1] >= lst[i][0]:
+    if right >= lst[i][0]:
         # 확장된다면?
-        if lst[i-1][1] < lst[i][1]:
-            length += lst[i][1]-lst[i-1][1]
+        if right < lst[i][1]:
+            length += lst[i][1] - right
+            right = lst[i][1]
         # 확장안되면?
         else:
-            new_tuple = (lst[i][0], lst[i-1][1])
-            lst[i] = new_tuple
+            pass
 
     # 안겹치면?
     else:
-        length += lst[i][1]-lst[i][0]
+        length += lst[i][1] - lst[i][0]
+        right = lst[i][1]
 print(length)
