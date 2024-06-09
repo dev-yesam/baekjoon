bdee6b7c2acc0c82fdb849aad696068798777267
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-06-09 10:36:06 +0900

    [Gold V] Title: 선 긋기, Time: 3604 ms, Memory: 172204 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.py
+++ 백준/Gold/2170. 선 긋기/선 긋기.py
@@ -2,10 +2,10 @@ import sys
 
 input = sys.stdin.readline
 
-## 신발끈 공식 써야함
+
 
 n = int(input())
-lst = [list(map(int, input().split())) for _ in range(n)]
+lst = [tuple(map(int, input().split())) for _ in range(n)]
 lst.sort()
 length = lst[0][1]-lst[0][0]
 for i in range(1,n):
@@ -16,7 +16,9 @@ for i in range(1,n):
             length += lst[i][1]-lst[i-1][1]
         # 확장안되면?
         else:
-            lst[i][1] = lst[i-1][1]
+            new_tuple = (lst[i][0], lst[i-1][1])
+            lst[i] = new_tuple
+
     # 안겹치면?
     else:
         length += lst[i][1]-lst[i][0]
