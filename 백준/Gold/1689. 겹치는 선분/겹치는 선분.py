6b1aa57d1e76a40f6cae399944208070a23eda84
Dev_Yesam <156279309+dev-yesam@users.noreply.github.com>
2024-06-09 13:04:23 +0900

    [Gold IV] Title: 겹치는 선분, Time: 5500 ms, Memory: 404352 KB -BaekjoonHub

--- 백준/Gold/1689. 겹치는 선분/겹치는 선분.py
+++ 백준/Gold/1689. 겹치는 선분/겹치는 선분.py
@@ -1,5 +1,4 @@
 import sys
-from collections import deque
 
 input = sys.stdin.readline
 
@@ -11,14 +10,10 @@ for _ in range(n):
     lst.append((e, -1))  # 끝점
 
 lst.sort(key=lambda x: (x[0], x[1]))
-ans_lst = []
 ans = 0
 count = 0
 for x, y in lst:
-    if y == 1 : # 시작점이면
-        count+=1
-        ans = max(ans,count)
-    else: # 끝점이면
-        count -= 1
+    count += y
+    ans = max(ans, count)
 
 print(ans)
